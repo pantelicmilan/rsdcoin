b8bafa719fdd27abb838d0304c1aac01c0d0d05f
Milan Pantelic <mpantelic39@gmail.com>
2018-02-18 19:33:51 +0100

    First official launch of a Digital Serbian Dinar
    Glitch/Unended version
    Developer : Milan Pantelic

---
+++ src/rsdcoindesing/Rsdcoindesing.java
@@ -0,0 +1,30 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsdcoindesing;
+import javax.swing.*;
+
+/**
+ *
+ * @author mpant
+ */
+public class Rsdcoindesing {
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       
+        Register form = new Register();
+        form.setVisible(true);
+       
+    }
+    
+}
